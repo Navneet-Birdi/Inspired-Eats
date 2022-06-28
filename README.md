f4bc244ebbed8a4472af2c4e388e22d38122f516
Sara-M <sara.monintja@gmail.com>
2022-06-28 13:43:56 +0800

    added media query + changes on css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Inspired-Eats
+
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -108,40 +108,6 @@ $(searchBtn).click(function () {
 
         // GET WARM WEATHER RECIPES
 
-<<<<<<< HEAD
-// Weather API
-const weatherKey = "af8df023e716fa9cc10ee93697bcbff5";
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=perth&appid=${weatherKey}&units=metric`)
-.then(function (result) {
-const weatherPromise = result.json()
-return weatherPromise
-})
-.then(function (weatherPromise) {
-    const weatherIcon = $("<img>")
-   
-    const temp = weatherPromise.main.temp
-    const conditions = weatherPromise.weather[0].main
-
-
-    $(weatherEL).text(temp + "° " + conditions)
-    console.log(temp, conditions)
-    
-    if(temp < 25) {
-        $(weatherIcon).attr("src", "./assets/imgs/icons/cold.png")
-        $(weatherIcon).attr("height", "50px")
-        $(iconEl).append(weatherIcon)
-    } else {
-        $(weatherIcon).attr("src", "./assets/imgs/icons/warm.png")
-        $(weatherIcon).attr("height", "50px")
-        $(iconEl).append(weatherIcon)
-    }
-    
-})
-
-
-
-=======
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=american`, {
           mode: "cors",
         })
@@ -172,4 +138,3 @@ return weatherPromise
       }
     });
 });
->>>>>>> b379fc69d3cf30f216a1a476d35cdcc5a0180a81
